d414c1851a037fa2b877d24b566637da07e03293
Michal <michalli@edu.hac.ac.il>
2022-03-26 15:12:36 +0300

    Added files

---
+++ thats-the-way.py
@@ -0,0 +1,24 @@
+import os
+
+"""
+In this program I created a function that gets a path to a directory and returns a 
+list of all files in the given directory which start with the word 'deep'
+"""
+
+
+# This function gets a path to a directory and returns a list of the files that start
+# with the word 'deep' in the given directory
+def get_deep_files(path):
+    """
+    :param path: a path to the directory we would like to check
+    :return: a list of names of the files that start with the word 'deep'
+    """
+    my_list = list()
+    for x in os.listdir(path):
+        if x.startswith("deep"):
+            my_list.append(x)
+
+    return my_list
+
+
+print('\n'.join(get_deep_files("C:\\Users\\Michal\\Downloads\\Notebooks-master\\week05\\images")))
