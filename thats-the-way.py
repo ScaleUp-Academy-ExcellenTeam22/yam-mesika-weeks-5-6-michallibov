a333567a742719f1669289c6dc2b6fb3464eeb8a
michallibov <92799624+michallibov@users.noreply.github.com>
2022-03-27 18:30:12 +0300

    Update thats-the-way.py
    Changed variable's names, added main and corrected the documentation.

--- thats-the-way.py
+++ thats-the-way.py
@@ -6,19 +6,21 @@ list of all files in the given directory which start with the word 'deep'
 """
 
 
-# This function gets a path to a directory and returns a list of the files that start
-# with the word 'deep' in the given directory
 def get_deep_files(path):
     """
+    This function gets a path to a directory and returns a list of the files that start
+    with the word 'deep' in the given directory.
     :param path: a path to the directory we would like to check
     :return: a list of names of the files that start with the word 'deep'
     """
-    my_list = list()
-    for x in os.listdir(path):
-        if x.startswith("deep"):
-            my_list.append(x)
+    files_start_with_deep_list = list()
+    for file_name in os.listdir(path):
+        if file_name.startswith("deep"):
+            files_start_with_deep_list.append(file_name)
 
-    return my_list
+    return files_start_with_deep_list
 
 
-print('\n'.join(get_deep_files("C:\\Users\\Michal\\Downloads\\Notebooks-master\\week05\\images")))
+if __name__ == '__main__':
+    print('\n'.join(get_deep_files("C:\\Users\\Michal\\Downloads\\Notebooks-master\\week05\\images")))
+    
