a7320655b8674728672535d63d23db2a26002274
michallibov <92799624+michallibov@users.noreply.github.com>
2022-03-27 23:05:53 +0300

    Update piece-of-cake.py
    Corrected the format of the documentation, added main and used reduced function

--- piece-of-cake.py
+++ piece-of-cake.py
@@ -1,25 +1,22 @@
-"""
-In this program I created a function that gets a dictionary of an ingredient and it's price,
-an optional list of items that we can ignore and the amount of each ingredient we must buy.
-The program will calculate the sum of all the prices of the needed ingredients based on their
-amount and price and will return the price of everything.
-"""
+import functools
 
 
-# This function gets ingredients, their prices, optional ingredients and how much of them
-# we need, and calculates the total price of the ingredients we must buy.
 def get_recipe_price(prices, optionals=[], **ingredients):
     """
+    This function gets ingredients, their prices, optional ingredients and how much of them
+    we need, and calculates the total price of the ingredients we must buy.
     :param prices: a dictionary of the names and prices of an ingredient
     :param optionals: a list of optional ingredients we don't have to buy
     :param ingredients: the amount of each ingredient we need
     :return: the total price we will have to pay
     """
-    total_price = 0
-    for ingredient in ingredients:
-        if ingredient not in optionals:     # if we must buy this ingredient (doesn't appear in optional)
-            total_price += prices.get(ingredient) * ingredients.get(ingredient) // 100
+
+    # Sums up the total price of all ingredients we must buy using a lambda function.
+    total_price = functools.reduce(lambda price, ingredient: price + ingredients.get(ingredient) *
+                                    prices.get(ingredient)//100 if ingredient not in optionals else price, prices, 0)
+
     return total_price
 
 
-print(get_recipe_price({'chocolate': 18, 'milk': 8}, milk=100, chocolate=200))
+if __name__ == '__main__':
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, milk=100, chocolate=200))
