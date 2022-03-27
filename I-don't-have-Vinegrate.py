e4bfedc0c1561eb950073694ee7b9f1c246ce98e
michallibov <92799624+michallibov@users.noreply.github.com>
2022-03-27 18:24:19 +0300

    Update I-don't-have-Vinegrate.py
    Changed variables names, added main and fixed the documentation.

--- I-don't-have-Vinegrate.py
+++ I-don't-have-Vinegrate.py
@@ -9,60 +9,70 @@ date.
 Note- you can only enter dates after the first unix epoch second which was at- 00:00:00 UTC on 1 January 1970
 """
 
-# This function converts the random timestamp to a date and prints the day of the week of the
-# randomized date, or if it's day is monday- it will print 'I'm out of Vinegret!'.
-def print_solution(randed):
-    """
-    :param randed: the randed date
-    """
-    new_date = datetime.datetime.fromtimestamp(randed)
-    day = datetime.datetime.weekday(new_date)
 
+def print_solution(day):
+    """
+    This function prints 'I'm out f Vinegret!' if the randed
+    date's day is Monday or the day itself if the day is not
+    Monday.
+    :param day: the day of the randed date
+    """
     if calendar.day_name[day] == 'Monday':
         print("I'm out of Vinegret!")
     else:
         print(calendar.day_name[day])
 
 
-# This function randomizes a number between the two timestamps which will
-# be later converted to a date that is also going to be between the two
-# given dates.
 def randomize(first_timestamp, second_timestamp):
     """
+    This function randomizes a number between the two timestamps which will
+    be later converted to a date that is also going to be between the two
+    given dates.
     :param first_timestamp: the first date we received from the user converted to timestamp
     :param second_timestamp: the second date we received from the user converted to timestamp
+    :return the randomized date's day
     """
-    if first_timestamp < second_timestamp:     # the first timestamp's date is before the second timestamp's date
+
+    # The first timestamp's date is before the second timestamp's date
+    if first_timestamp < second_timestamp:
         randed = random.randint(first_timestamp, second_timestamp)
-    else:                           # the second timestamp's date is before the first timestamp's date
+
+    # The second timestamp's date is before the first timestamp's date
+    else:
         randed = random.randint(second_timestamp, first_timestamp)
-    print_solution(randed)
+
+    new_date = datetime.datetime.fromtimestamp(randed)
+    day = datetime.datetime.weekday(new_date)
+    return day
 
 
-# This function converts the datetime variables to timestamps.
 def convert_to_timestamp(first_date, second_date):
     """
+    This function converts the datetime variables to timestamps.
     :param first_date: the first date we received from the user
     :param second_date:  the second date we received from the user
+    :return the return value of the randomize function which is the
+            randomized date's day
     """
     timestamp1 = datetime.datetime.timestamp(first_date)
     timestamp2 = datetime.datetime.timestamp(second_date)
     timestamp1 = int(timestamp1)
     timestamp2 = int(timestamp2)
-    randomize(timestamp1, timestamp2)
+    return randomize(timestamp1, timestamp2)
 
 
-# This function converts a given date string to a datetime variable.
-def convert_to_date(f_date, s_date):
+def convert_to_date(first_date, second_date):
     """
-    :param f_date: the first date we received from the user in a form of string
-    :param s_date: the second date we received from the user in a form of string
+    This function converts a given date string to a datetime variable.
+    :param first_date: the first date we received from the user in a form of string
+    :param second_date: the second date we received from the user in a form of string
     """
-    date1 = datetime.datetime.strptime(f_date, "%Y-%m-%d")
-    date2 = datetime.datetime.strptime(s_date, "%Y-%m-%d")
-    convert_to_timestamp(date1, date2)
+    first_converted_date = datetime.datetime.strptime(first_date, "%Y-%m-%d")
+    second_converted_date = datetime.datetime.strptime(second_date, "%Y-%m-%d")
+    return convert_to_timestamp(first_converted_date, second_converted_date)
 
 
-first_date = input("Enter first date: ")
-second_date = input("Enter second date: ")
-convert_to_date(first_date, second_date)
+if __name__ == '__main__':
+    first_date = input("Enter first date: ")
+    second_date = input("Enter second date: ")
+    print_solution(convert_to_date(first_date, second_date))
